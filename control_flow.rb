4c72e93f30d46031990c4211be827599c3ef3a48
beckireby15@gmail.com <beckireby15@gmail.com>
2022-10-20 20:58:43 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,76 @@
 def admin_login(username, password)
   # your code here
+
+
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+
+    "Access granted"
+
+  else
+    "Access denied"
+  
+
 end
+end
+
 
 def hows_the_weather(temperature)
   # your code here
+
+ 
+
+response = if (temperature < 40)
+
+      "brisk"
+
+      elsif (temperature >= 40 && temperature <= 65 )
+        "a little chilly"
+        
+        elsif(temperature > 85)
+          "too dang hot"
+
+          else
+            "perfect"
+            end
+
+            "It's #{response} out there!"
+
 end
 
 def fizzbuzz(num)
+
+  if num % 3 === 0 && num % 5 === 0
+    return "FizzBuzz"
+
+    elsif num % 3 ===0
+      return "Fizz"
+
+      elsif num % 5 ===0
+        return "Buzz"
+
+        else num
+          end
+
+
   # your code here
 end
 
 def calculator(operation, num1, num2)
-  # your code here
-end
 
+case operation
+when "+"
+  num1 + num2
+
+  when "-"
+    num1 - num2
+
+    when "*"
+      num1 * num2
+
+      when "/"
+        num1 / num2
+
+        else
+          puts "Invalid operation!"
+end
+end
